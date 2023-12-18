d5dd9135bda576b86808d57912098dceaa0d8335
simon0820s <simon0820s@gmail.com>
2023-12-17 22:46:26 -0500

    create constructor

--- perceptron/src/create_perceptron.rs
+++ perceptron/src/create_perceptron.rs
@@ -4,3 +4,14 @@ struct Perceptron {
   weights: Vec<f64>,
   learning_rate: f64
 }
+
+impl Perceptron {
+  fn new(input_size: usize, learning_rate: f64) -> Perceptron {
+    let mut rng = thread_rng();
+    let weights: Vec<f64> = (0..input_size).map(|_| rng.gen_range(-1.0..1.0)).colletct();
+    Perceptron {
+      weights,
+      learning_rate,
+    }
+  }
+}
