cd8aa8a1930fdf432421b80408c3dd9892f081ba
simon0820s <simon0820s@gmail.com>
2023-12-20 10:01:24 -0500

    conversor perceptron understand linear conversions

--- conversor_perceptron/src/main.rs
+++ conversor_perceptron/src/main.rs
@@ -1,6 +1,54 @@
 mod perceptron;
 use perceptron::Perceptron;
+
+const TRAIN_DATA: [[f64; 2]; 10] = [
+    [0.0, 32.0],
+    [5.0, 41.0],
+    [10.0, 50.0],
+    [15.0, 59.0],
+    [20.0, 68.0],
+    [25.0, 77.0],
+    [30.0, 86.0],
+    [35.0, 95.0],
+    [40.0, 104.0],
+    [45.0, 113.0],
+];
+
 fn main() {
-    let perceptron = Perceptron::new(0.01);
-    println!("w: {} lr: {}", perceptron.get_weight(), perceptron.get_learning_rate());
+    println!("True data => {:?}", TRAIN_DATA);
+    println!();
+    let mut perceptron = Perceptron::new(0.0001);
+    println!("Before train => ");
+    println!(
+        "w: {} lr: {} b: {}",
+        perceptron.get_weight(),
+        perceptron.get_learning_rate(),
+        perceptron.get_bias()
+    );
+    println!(
+        "Predictions before train: [0, {}] [40, {}], [-2, {}]",
+        perceptron.predict(0.0),
+        perceptron.predict(40.0),
+        perceptron.predict(-2.0)
+    );
+    println!();
+    for _ in 0..100000 {
+        for i in 0..TRAIN_DATA.len() {
+            perceptron.train(TRAIN_DATA[i]);
+        }
+    }
+
+    println!("After train => ");
+    println!(
+        "w: {} lr: {} b: {}",
+        perceptron.get_weight(),
+        perceptron.get_learning_rate(),
+        perceptron.get_bias()
+    );
+    println!(
+        "Predictions after train: [0, {}] [40, {}], [-2, {}]",
+        perceptron.predict(0.0),
+        perceptron.predict(40.0),
+        perceptron.predict(-2.0)
+    )
 }
--- conversor_perceptron/src/perceptron.rs
+++ conversor_perceptron/src/perceptron.rs
@@ -1,6 +1,7 @@
 use rand::{thread_rng, Rng};
 pub struct Perceptron {
   weight: f64,
+  bias: f64,
   learning_rate: f64
 }
 
@@ -9,9 +10,25 @@ impl Perceptron {
     pub fn new(learning_rate: f64) -> Perceptron {
       let mut rng = thread_rng();
       let weight: f64 = rng.gen_range(0.0..1.0);
+      let bias: f64 = rng.gen_range(0.0..1.0);
       Perceptron {
         weight,
+        bias,
         learning_rate
+      }
+    }
+
+    pub fn predict(&self, input: f64) -> f64 {
+      input * self.weight + self.bias
+    }
+
+    pub fn train(&mut self, train_data: [f64; 2]){
+      let guess = self.predict(train_data[0]);
+      let error = train_data[1] - guess;
+
+      if error != 0.0 {
+        self.weight += self.learning_rate * error * train_data[0];
+        self.bias += self.learning_rate * error
       }
     }
 
@@ -21,6 +38,10 @@ impl Perceptron {
 
     pub fn get_learning_rate(&self) -> f64 {
       self.learning_rate
+    }
+
+    pub fn get_bias(&self) -> f64 {
+      self.bias
     }
 
 }
