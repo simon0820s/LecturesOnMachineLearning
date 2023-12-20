7d064d42f2e66fe39c6d7858234424694ae11596
simon0820s <simon0820s@gmail.com>
2023-12-20 09:08:44 -0500

    create if conversor

---
+++ conversor_perceptron/target/debug/deps/conversor_perceptron-277d487bf65529e4.d
@@ -0,0 +1,5 @@
+/home/simon/programacion/ml/conversor_perceptron/target/debug/deps/libconversor_perceptron-277d487bf65529e4.rmeta: src/main.rs
+
+/home/simon/programacion/ml/conversor_perceptron/target/debug/deps/conversor_perceptron-277d487bf65529e4.d: src/main.rs
+
+src/main.rs:
---
+++ conversor_perceptron/target/debug/deps/conversor_perceptron-db3d82aeac01aead.d
@@ -0,0 +1,5 @@
+/home/simon/programacion/ml/conversor_perceptron/target/debug/deps/libconversor_perceptron-db3d82aeac01aead.rmeta: src/main.rs
+
+/home/simon/programacion/ml/conversor_perceptron/target/debug/deps/conversor_perceptron-db3d82aeac01aead.d: src/main.rs
+
+src/main.rs:
