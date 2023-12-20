0a51b140e6c54cb5141307f4e317c88449510a41
simon0820s <simon0820s@gmail.com>
2023-12-20 09:04:52 -0500

    create new project of conversor_perceptron

---
+++ conversor_perceptron/target/debug/deps/conversor_perceptron-499c9e9a2202a2dc.d
@@ -0,0 +1,5 @@
+/home/simon/programacion/ml/conversor_perceptron/target/debug/deps/conversor_perceptron-499c9e9a2202a2dc: src/main.rs
+
+/home/simon/programacion/ml/conversor_perceptron/target/debug/deps/conversor_perceptron-499c9e9a2202a2dc.d: src/main.rs
+
+src/main.rs:
